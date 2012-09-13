2560d1f8632925d29d2d04dbc57198e857cfd7ac
Lars Thorup <lars@zealake.com>
2012-09-13 16:50:03 -0700

    moved from svn

---
+++ source/password.js
@@ -0,0 +1,33 @@
+PasswordEvaluator = function() {
+};
+
+PasswordEvaluator.prototype = {
+    strength: function(password) {
+        var result = 0;
+        if(password.length > 4) {
+            ++result;
+        }
+        if(password.length > 8) {
+            ++result;
+        }
+        var digitCount = (password.match(/\d/g) || []).length;
+        var alphaCount = (password.match(/[a-zA-Z]/g) || []).length;
+        var specialCount = password.length - digitCount - alphaCount;
+        var categoryCount = 0;
+        if(digitCount > 0) ++categoryCount;
+        if(alphaCount > 0) ++categoryCount;
+        if(specialCount > 0) ++categoryCount;
+        if(categoryCount > 1) {
+            ++ result;
+        }
+        if(categoryCount > 2) {
+            ++result;
+        }
+        var lowerCount = (password.match(/[a-z]/g) || []).length;
+        var upperCount = (password.match(/[A-Z]/g) || []).length;
+        if(lowerCount > 0 && upperCount > 0) {
+            ++result;
+        }
+        return result;
+    }
+};
---
+++ test.cmd
@@ -0,0 +1,1 @@
+build\phantomjs.exe build\run_qunit.js file:///C:/repositories/zealake/2012/tddjs-agileinstitute-2012/code/source/password.test.html junit-xml > testresult.xml
