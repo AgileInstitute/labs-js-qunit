101aa89ec5fd1740b05983e20a609c98cbbe865b
Lars Thorup <lars@zealake.com>
2012-10-27 13:42:23 -0700

    currency converter - isolate behaviour

---
+++ source/CurrencyConversion/CC12/currencyConverter.js
@@ -0,0 +1,32 @@
+CurrencyConverter = function() {
+};
+
+CurrencyConverter.prototype = {
+    convert: function(value, from, to) {
+        if(to === from) {
+            return value;
+        }
+        return value * this.conversionRate(from, to);
+    },
+    conversionRate: function(from, to) {
+        return 0.5000;
+    }
+};
+
+ConversionRates = function() {
+    this.rates = [];
+};
+
+// Begin slide 114 (Isolate Behavior)
+ConversionRates.prototype = {
+    putRate: function(from, to, rate) {
+        this.rates[this.key(from, to)] = rate;
+    },
+    getRate: function(from, to) {
+        return this.rates[this.key(from, to)];
+    },
+    key: function(from, to) {
+        return from + to;
+    }
+};
+// End slide 114
