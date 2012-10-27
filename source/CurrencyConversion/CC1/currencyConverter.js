e8d655649805b46940d0b4af7236ca7ea5f446b7
Lars Thorup <lars@zealake.com>
2012-10-27 09:16:46 -0700

    currency converter step 1

--- source/CurrencyConversion/CC1/currencyConverter.js
+++ source/CurrencyConversion/CC1/currencyConverter.js
@@ -1,8 +1,10 @@
 CurrencyConverter = function() {
 };
 
+// Begin slide 92 ("Thank You, But..")
 CurrencyConverter.prototype = {
     convert: function(value, from, to) {
         throw "D'oh! convert() not YET implemented!";
     }
 };
+/ /End slide 92
