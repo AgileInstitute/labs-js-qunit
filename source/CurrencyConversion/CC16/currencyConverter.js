06eabfad0bd6e3344377047e2296e948d5b8c3df
Lars Thorup <lars@zealake.com>
2012-10-27 14:29:22 -0700

    currency converter - expressive exception

---
+++ source/CurrencyConversion/CC16/currencyConverter.js
@@ -0,0 +1,35 @@
+CurrencyConverter = function(rates) {
+    this.rates = rates;
+};
+
+CurrencyConverter.prototype = {
+    convert: function(value, from, to) {
+        if(to === from) {
+            return value;
+        }
+        return value * this.conversionRate(from, to);
+    },
+    conversionRate: function(from, to) {
+        return this.rates.getRate(from, to);
+    }
+};
+
+ConversionRates = function() {
+    this.rates = [];
+};
+
+ConversionRates.prototype = {
+    putRate: function(from, to, rate) {
+        this.rates[this.key(from, to)] = rate;
+    },
+    getRate: function(from, to) {
+        var rate = this.rates[this.key(from, to)];
+        if(rate === undefined) {
+            throw "rate not found from '" + from + "' to '" + to + "'";
+        }
+        return rate;
+    },
+    key: function(from, to) {
+        return from + to;
+    }
+};
