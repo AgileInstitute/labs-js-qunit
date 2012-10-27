e5e7d65c669d86f75f311538af05ba7791b5bcf6
Lars Thorup <lars@zealake.com>
2012-10-27 12:26:50 -0700

    currency converter - still fake

---
+++ source/CurrencyConversion/CC5/currencyConverter.js
@@ -0,0 +1,10 @@
+CurrencyConverter = function() {
+};
+
+// Begin slide 99 (Still "Fake"?)
+CurrencyConverter.prototype = {
+    convert: function(value, from, to) {
+        return value;
+    }
+};
+// End slide 99
