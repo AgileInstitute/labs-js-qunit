af418d31db901b96ae6760201351e4063496d76e
Rob Myers <Rob.Myers@agileInstitute.com>
2012-11-02 12:50:07 -0700

    AJAX sample changed - LT/RM
    model.js: renamed success argument
    tests: explanatory comments, removed unwanted asyncTest argument (only
    needed for multiple callbacks)

--- source/model.js
+++ source/model.js
@@ -5,11 +5,11 @@ Model.prototype = {
   getProduct: function(productId, callback) {
       jQuery.ajax("/product", {
           data: { productId : productId },
-          success: function(data) {
-              if(data.error) {
-                  callback(data.error);
+          success: function(response) {
+              if(response.error) {
+                  callback(response.error);
               } else {
-                  callback(null, data.product);
+                  callback(null, response.product);
               }
           },
           error: function() {
