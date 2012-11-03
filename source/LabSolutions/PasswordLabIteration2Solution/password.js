6e71361b605c7155fb56c798617af3e94a8273c4
Lars Thorup <lars@zealake.com>
2012-11-02 17:21:37 -0700

    fixing broken build

--- source/LabSolutions/PasswordLabIteration2Solution/password.js
+++ source/LabSolutions/PasswordLabIteration2Solution/password.js
@@ -4,25 +4,26 @@ PasswordEvaluator = function () {
 
 PasswordEvaluator.validationResult = function (valid, message) {
     return { valid: valid, message: message };
-}
+};
 
 PasswordEvaluator.longEnough = function (password) {
     return PasswordEvaluator.validationResult(password.length > 4, "more than 4 characters");
-}
+};
 
 PasswordEvaluator.mixedCategories = function (password) {
     var digitCount = (password.match(/\d/g) || []).length;
     var alphaCount = (password.match(/[a-zA-Z]/g) || []).length;
     return PasswordEvaluator.validationResult(digitCount > 0 && alphaCount > 0, "mix of letters and digits");
-}
+};
 
 PasswordEvaluator.prototype = {
     evaluate: function (password) {
         var failedRules = [];
-        for(var i = 0; i < this.rules.length; ++i) {
+        var i;
+        for (i = 0; i < this.rules.length; i += 1) {
             var rule = this.rules[i];
             var result = rule(password);
-            if(!result.valid) {
+            if (!result.valid) {
                 failedRules.push(result.message);
             }
         }
